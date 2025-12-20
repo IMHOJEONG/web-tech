eb0e4843f8440b4a4d0a5d8193e52ee6c1d87901
IMHOJEONG <39ghwjd@naver.com>
2025-12-21 09:23:04 +1100

    fix: fix web - tailwind.config.ts remove

--- apps/web/postcss.config.mjs
+++ apps/web/postcss.config.mjs
@@ -1,6 +1,3 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-export default config;
+import { postcssConfig } from "@web-tech/tailwind-config/postcss";
+
+export default postcssConfig;
