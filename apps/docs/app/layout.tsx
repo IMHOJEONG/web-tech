23930c2ff937a1e5d2a60e8a6dc85ae1e2507bfb
IMHOJEONG <39ghwjd@naver.com>
2026-06-15 23:59:58 +0900

    fix: change favicon, logo icon

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -78,6 +78,12 @@ export default async function Layout({
                         strategy="beforeInteractive"
                     />
                 )}
+                <link
+                    rel="icon"
+                    href="/favicon.png"
+                    type="image/<generated>"
+                    sizes="<generated>"
+                />
             </head>
             <NextIntlClientProvider locale={locale} messages={messages}>
                 <body
